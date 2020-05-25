9ee99e753285d219847e654727054b088be4de09
Maciej Pastor <maciej.pastor@gmail.com>
2020-05-25 16:54:22 +0200

    tablica

--- src/Engine/Board.java
+++ src/Engine/Board.java
@@ -2,17 +2,17 @@ package Engine;
 
 public class Board {
     public static void main(String[] args) {
-        String [][] planszapoziom = new String [8][8];
+        Object [][] planszapoziom = new Object[8][8];
 
 
         planszapoziom [2][2] = "a kuku";
 
 
         for(int i = 0; i <planszapoziom.length; i++) {
-            String [] planszapion = planszapoziom[i];
+            Object [] planszapion = planszapoziom[i];
             for (int j = 0; j< planszapion.length; j++) {
 
-                planszapoziom[i][j] = "O";
+//                planszapoziom[i][j] = "O";
                 System.out.print(planszapoziom[i][j] + " ");
 
             }
